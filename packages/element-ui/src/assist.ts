394c98b3a063d45b2899960d1c6d87776f44c5fc
xiaohaih <773884562@qq.com>
2025-05-12 11:09:55 +0800

    :construction: 更新 AssistOption 类型声明, 支持主动声明时可声明未在配置中的字段, 支持回调中的 query 和 options 可读取未声明的字段

--- packages/element-ui/src/assist.ts
+++ packages/element-ui/src/assist.ts
@@ -3,17 +3,18 @@ import { reactive } from 'vue-demi';
 import type * as JSONFormTs from './interface';
 
 /** 当泛型为多个参数时, 辅助推断类型 */
-type AssistOption<T extends Record<string, any>, O extends Record<keyof T, any>> = {
+type AssistOption<T extends Record<string, any>, O extends Partial<Record<keyof T, any>>> = {
     // 推断出 query 的值(经过 PropType 的转换, 会导致 ts 丢失实际的值)
-    // [K in keyof T]: JSONFormTs.JSONFormOption<T[K], T, O[K], O>;
+    // 在不主动补充声明的情况下, 会自动推断 options 值, 因此第一个参数用在 options 上, 第二个参数用在 query 上
+    // [K in keyof T]?: JSONFormTs.JSONFormOption<T[K], T, O[K], O>;
     // 推断出 options 的值
-    [K in keyof T]: JSONFormTs.JSONFormOption<O[K], O, T[K], objAttr2Arr<T>> | FalsyType;
+    [K in keyof T]?: JSONFormTs.JSONFormOption<O[K], O & Record<string | symbol | number, any>, T[K], objAttr2Arr<T & Record<string | symbol | number, any>>> | FalsyType;
 };
 /** 将对象中的值转为数组 */
 type objAttr2Arr<T> = { [K in keyof T]: T[K][] };
 /** 当泛型为一个参数时, 辅助推断类型 */
 type Assist2Option<T extends Record<string, Record<'value' | 'options', any>>> = {
-    [K in keyof T]: JSONFormTs.JSONFormOption<T[K]['value'], Option2Obj<T, 'value'>, T[K]['options'], objAttr2Arr<Option2Obj<T, 'options'>>> | FalsyType;
+    [K in keyof T]?: JSONFormTs.JSONFormOption<T[K]['value'], Option2Obj<T, 'value'>, T[K]['options'], objAttr2Arr<Option2Obj<T, 'options'>>> | FalsyType;
 };
 /** 提取内部的值 */
 type Option2Obj<T extends Record<string, Record<'value' | 'options', any>>, J extends 'value' | 'options'> = {
@@ -23,7 +24,7 @@ type Option2Obj<T extends Record<string, Record<'value' | 'options', any>>, J ex
 type FalsyType = number | boolean | string | null | undefined;
 
 /** 定义配置项 */
-export function defineOption<T extends Record<string, any>, O extends Record<keyof T, any>>(config: AssistOption<T, O>): AssistOption<T, O>;
+export function defineOption<T extends Record<string, any>, O extends Partial<Record<keyof T, any>>>(config: AssistOption<T, O>): AssistOption<T, O>;
 export function defineOption<T extends Record<string, Record<'value' | 'options', any>>>(config: Assist2Option<T>): Assist2Option<T>;
 export function defineOption(config: any) {
     return reactive(config);
@@ -38,24 +39,29 @@ export function defineOption(config: any) {
 //         // placeholder: '',
 //         // options: [{ label: '123', value: 123 }],
 //         // options: [(new Map())],
-//         // options: { a: 1 },
-//         // options: [{ label: '' }],
+//         // options: [{ a: 1 }],
+//         options: [{ label: '' }],
 //         // itemSlots: {},
 //         getOptions(cb, query, option) {
-//             // option.options.ab; // ab 的类型
+//             const r = option.options.ab; // ab 的类型
+//             console.log(r);
 //         },
 //     },
-//     // cc: {
-//     //     t: 'custom-render',
-//     //     // defaultValue: 111,
-//     //     // getOptions(cb, query, option) {
-//     //     // },
-//     //     render() {
-//     //         return () => {};
-//     //     },
-//     // },
+//     cc: {
+//         t: 'custom-render',
+//         // defaultValue: 111,
+//         // getOptions(cb, query, option) {
+//         // },
+//         render() {
+//             return () => {};
+//         },
+//     },
 // });
-// defineOption<{ ab: { value: string; options: { label: number; value: number } }; cc: { value: number; options: string } }>({
+// defineOption<{
+//     ab: { value: string; options: { label: number; value: number } };
+//     cc: { value: number; options: string };
+//     bb: { value: string; options: Record<string, string> };
+// }>({
 //     ab: {
 //         t: 'input',
 //         fields: [],
@@ -64,14 +70,44 @@ export function defineOption(config: any) {
 //         options: [{ label: 123, value: 123 }],
 //         itemSlots: {},
 //         getOptions(cb, query, option) {
-//             // option.options.ab; // ab 的类型
+//             const r = option.options.ab; // ab 的类型
+//             console.log(r);
 //         },
 //     },
 //     cc: {
 //         t: 'custom-render',
 //         defaultValue: 111,
 //         getOptions(cb, query, option) {
-//             // option.options.ab; // ab 的类型
+//             const r = option.options.cc; // cc 的类型
+//             console.log(r);
+//         },
+//         render() {
+//             return () => {};
+//         },
+//     },
+// });
+// defineOption<
+//     { ab: { label: number; value: number }; cc: string; bb: Record<string, string> },
+//     { ab: string; cc: number }
+// >({
+//     ab: {
+//         t: 'input',
+//         fields: [],
+//         defaultValue: '123,',
+//         placeholder: '',
+//         options: [{ label: 123, value: 123 }],
+//         itemSlots: {},
+//         getOptions(cb, query, option) {
+//             const r = option.options.ab; // ab 的类型
+//             console.log(r);
+//         },
+//     },
+//     cc: {
+//         t: 'custom-render',
+//         defaultValue: 111,
+//         getOptions(cb, query, option) {
+//             const r = option.options.cc; // ab 的类型
+//             console.log(r);
 //         },
 //         render() {
 //             return () => {};
